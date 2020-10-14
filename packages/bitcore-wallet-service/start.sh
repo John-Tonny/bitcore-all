b1ee1c09d9a4efd7a4f9829a9767f884fb5abcbf
John-Tonny <szlhao@tom.com>
2020-10-13 20:50:24 -0700

    update bitcore-wallet-service/start.sh

--- packages/bitcore-wallet-service/start.sh
+++ packages/bitcore-wallet-service/start.sh
@@ -35,5 +35,5 @@ run_program ./ts_build/emailservice/emailservice.js pids/emailservice.pid logs/e
 run_program ./ts_build/pushnotificationsservice/pushnotificationsservice.js pids/pushnotificationsservice.pid logs/pushnotificationsservice.log
 run_program ./ts_build/fiatrateservice/fiatrateservice.js pids/fiatrateservice.pid logs/fiatrateservice.log
 run_program ./ts_build/masternodeservice/masternodeservice.js pids/masternodeservice.pid logs/masternodeservice.log
-#run_program ./ts_build/bws.js pids/bws.pid logs/bws.log
+run_program ./ts_build/bws.js pids/bws.pid logs/bws.log
 
